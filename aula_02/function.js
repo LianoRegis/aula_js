ae65e5aaddb6b59726d0955732c9db0f7fa4b0e1
LianoRegis <liano.regis@gmail.com>
2023-05-11 19:57:33 -0300

    funcoes, chamas de funcao dentro de funcao

---
+++ aula_02/function.js
@@ -0,0 +1,21 @@
+function calculo(n1, n2, n3) {
+    let resultado = (n1 + n2) - n3
+    console.log(resultado)
+}
+//calculo(10, 20, 5)
+
+function soma(n1, n2)  {
+    let resultado = n1 + n2
+    console.log('Soma:', resultado)
+
+    return resultado
+}
+//soma(123, 678)
+
+function media(n1, n2) {
+    //let resultado = (n1+n2)/2
+    let resultado = soma(n1, n2)/2
+    console.log('Media:', resultado)
+
+}
+media(123, 678)
